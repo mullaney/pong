631f3cc532e4ff9f974a46fd29afced6b71d97ad
Kevin Kelly <kvinklly@gmail.com>
2018-01-29 21:54:15 -0600

    add paddles and ball

--- main.lua
+++ main.lua
@@ -10,10 +10,21 @@ push = require 'push'
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+VIRTUAL_WIDTH = 640
+VIRTUAL_HEIGHT = 360
+
+PADDLE_DIST_FROM_EDGE = 20
+PADDLE_WIDTH = 6
+PADDLE_LENGTH = 45
+BALL_RADIUS = 5
+
 function love.load()
   love.graphics.setDefaultFilter('nearest', 'nearest')
 
-  love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+  smallFont = love.graphics.newFont('font.ttf', 24)
+  love.graphics.setFont(smallFont)
+
+  push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
     fullscreen = false,
     resizable = false,
     vsync = true
@@ -28,15 +39,15 @@ end
 
 function love.draw()
 
-  push.apply('start')
+  push:apply('start')
 
-  love.graphics.printf(
-    'Hello Pong!',
-    0,
-    WINDOW_HEIGHT / 2 - 6,
-    WINDOW_WIDTH,
-    'center'
-  )
+  love.graphics.clear(40, 45, 52, 255)
 
-  push.apply('end')
+  love.graphics.printf('Hello Pong!', 0, 20, VIRTUAL_WIDTH, 'center')
+
+  love.graphics.rectangle('fill', PADDLE_DIST_FROM_EDGE, PADDLE_LENGTH, PADDLE_WIDTH, PADDLE_LENGTH)
+  love.graphics.rectangle('fill', VIRTUAL_WIDTH - PADDLE_DIST_FROM_EDGE - PADDLE_WIDTH, VIRTUAL_HEIGHT - PADDLE_LENGTH * 2, PADDLE_WIDTH, PADDLE_LENGTH)
+  love.graphics.circle('fill', VIRTUAL_WIDTH / 2, VIRTUAL_HEIGHT / 2, BALL_RADIUS)
+
+  push:apply('end')
 end
